d7d2775fec0c73220eb1cd06354eb0b85aa83e13
LeclercqTom <tomleclercq2208@gmail.com>
2024-01-30 10:19:34 +0100

    modif

--- Simple_tf/provider.tf
+++ Simple_tf/provider.tf
@@ -10,9 +10,9 @@ terraform {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
     features {}
-    client_id       = "#{573e809e-c035-4e6b-a443-d9d159f55acb}"
-    client_secret   = "XfH8Q~D40Uo-mvb.1HQF96GudkDl.Y.-Q9Da4cjC"
-    tenant_id       = "02a775bc-748f-4f51-900a-308c4b588f94"
-    subscription_id = "a9f1a513-0637-45f8-b4de-813d8688b5a5"
+    client_id       = "#{my_client_id}#"
+    client_secret   = "#{my_client_secret}#"
+    tenant_id       = "#{my_tenant_id}#"
+    subscription_id = "#{my_subscription_id}#"
 
 }
